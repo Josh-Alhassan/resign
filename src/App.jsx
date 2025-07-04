f6578c599da9cb6e2593072bf1ee741d2f604f42
Josh-Alhassan <saintjosh26@gmail.com>
2025-07-05 00:15:06 +0100

    feat: add hero section component with list of testimonials

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import Navigation from "./components/Navigation";
+import Hero from "./components/Hero";
 
 function App() {
   return (
     <div className="app">
       <Navigation />
+      <Hero />
       <h1>HELOOOOOO</h1>
     </div>
   );
